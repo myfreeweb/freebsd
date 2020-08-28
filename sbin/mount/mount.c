3bdf4bc8df17e7cc36ba5ecbe56c8de111ee380e
Ed Maste <emaste@FreeBSD.org>
2020-08-28 11:38:33 -0400

    retire smbfs(5) and related utilities
    It supports only the obsolete SMB v1 protocol.
    
    TODO: ObsoleteFiles.inc

--- sbin/mount/mount.c
+++ sbin/mount/mount.c
@@ -123,7 +123,7 @@ use_mountprog(const char *vfstype)
 	unsigned int i;
 	const char *fs[] = {
 	"cd9660", "mfs", "msdosfs", "nfs",
-	"nullfs", "smbfs", "udf", "unionfs",
+	"nullfs", "udf", "unionfs",
 	NULL
 	};
 
